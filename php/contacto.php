f52ee81a4ce51cae034e292105caab97afbe58ce
AndresyNathan <Musically@yopmail.com>
2020-03-08 15:03:42 -0600

    Update contacto.php

--- php/contacto.php
+++ php/contacto.php
@@ -5,15 +5,13 @@ $email = $_POST["email"];
 $mensaje = $_POST["mensaje"];
 
 //datos para el correo
-$para = "andres150098@yopmail.com";
+$para = "musicallycr@yopmail.com";
 $asunto = "Nuevo Mensaje de $nombre";
 
-$mensaje = "
-Nombre del remitente: ".$nombre."
-Correo  :  ".$email."
-Mensaje :  ".$mensaje."
-";
+$cuerpo = "De: $nombre\n";
+$cuerpo = "Correo: $email\n";
+$cuerpo = "Mensaje: $mensaje\n";
 
-mail($para,$asunto,utf8_decode($mensaje));
-header('Location:NewMessage.html');
+mail($para,$asunto,utf8_decode($cuerpo));
+header('Location:NewMessage.html')
 ?>

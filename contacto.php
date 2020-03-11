434eba2a450329208dfd7b6e6a34b21e939a96a9
andrescode-98 <60664468+andrescode-98@users.noreply.github.com>
2020-03-11 14:39:30 -0600

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -1,15 +1,22 @@
 <?php
-
-$destinatario = 'musicallycr@yopmail.com';
-
 $nombre = $_POST['nombre'];
-$correo = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+$mail = $_POST['email'];
+$empresa = $_POST['mensaje'];
 
-$header = 'Enviado desde Musically.com';
-$mensajecompleto = $mensaje . "\nAtentamente: " . $nombre;
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-mail($destinatario,$correo,$mensajecompleto,$header);
-echo "<script>alert(\"location.href='NewMessage.html'\")</script>"
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
 
+$para = 'andres150098@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:NewMessage.html");
 ?>

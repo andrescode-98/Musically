d24061252011481a611176aeb55790003b315035
andrescode-98 <60664468+andrescode-98@users.noreply.github.com>
2020-03-11 14:30:32 -0600

    update contact

--- contacto.php
+++ contacto.php
@@ -1,28 +1,15 @@
 <?php
-$result="";
-if(isset($_POST['submit'])){
-    require 'php/PHPMailerAutoload.php';
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->Host='smtp.yopmail.com';
-    $mail->Port=587; // un puerto que no este bloqueado
-    $mail->SMTPAuth=true;
-    $mail->SMTPSecure='tls'; //hace que la conexion sea segura
-    $mail->Username= 'musicallycr@yopmail.com';
-    $mail->Password= '';
 
-    $mail->setFrom($_POST['email'],$_POST['nombre']);//obtiene correo y nombre del ususario
-    $mail->addAddress('musicallycr@yopmail.com');
-    $mail->addReplyTo($_POST['email'],$_POST['nombre']);
+$destinatario = 'musicallycr@yopmail.com';
 
-    $mail->isHTML(true);
-    $mail->Subject='Correo Enviado Por: '.$_POST['nombre'];
-    $mail->Body='Nombre: '.$_POST['nombre'].'<br>Email: '.$_POST['email'].'<br>Mensaje : '.$_POST['mensaje'];
+$nombre = $_POST['nombre'];
+$correo = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 
-    if(!$mail->send()){
-       $result = "Algo salio mal, Intentalo de nuevo.";
-    }else{
-        $result="location:NewMessage.html";
-    }
-}
+$header = 'Enviado desde Musically.com';
+$mensajecompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+mail($destinatario,$correo,$mensajecompleto,$header);
+echo "<script>alert(\"location.href=NewMessage.html'\")</script>"
+
 ?>

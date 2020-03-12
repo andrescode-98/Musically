bf4c4984e6a11a1bc619d2555f069df8d2920103
AndresyNathan <Musically@yopmail.com>
2020-03-11 20:34:51 -0600

    Update contacto.php

--- contacto.php
+++ contacto.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'andres150098@gmail.com';
+$para = 'musicallycr@yopmail.com';
 $asunto = 'Mensaje de Musically.com';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
